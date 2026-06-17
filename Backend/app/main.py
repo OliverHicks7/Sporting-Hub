bdd11a9ebabf9128ee03e5afa3b73c2db830871e
OliverHicks7 <90759216+OliverHicks7@users.noreply.github.com>
2026-06-17 16:09:38 +0100

    Added User Creation Fucntionality
    Added db session and function to create user in Users table

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from app.database import engine
 from sqlalchemy import text
 from app import models
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.database import get_db
+from app.models import User
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,3 +23,11 @@ def db_check():
             return {"db": "connected", "result": result.scalar()}
     except Exception as e:
         return {"db": "failed", "error": str(e)}
+    
+@app.post("/users")
+def create_user(username: str, favourite_sport: str, db: Session = Depends(get_db)):
+    user = User(username=username, favourite_sport=favourite_sport)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
